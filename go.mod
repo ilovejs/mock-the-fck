6426ec7bc7a8409203ae6072368e8c8c8ffd38ac
Mic Zhuang <zhuangdeyouxiang@gmail.com>
2022-01-22 16:19:46 +1100

    add uber-fx DI demo

--- go.mod
+++ go.mod
@@ -6,18 +6,19 @@ require (
 	github.com/aws/aws-sdk-go v1.42.38
 	github.com/golang/mock v1.6.0
 	github.com/stretchr/testify v1.7.0
-	// note: this is required later
-	github.com/golang/mock v1.6.0
+	go.uber.org/fx v1.16.0
+
 )
 
 require (
-	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/objx v0.1.0 // indirect
-	golang.org/x/mod v0.4.2 // indirect
-	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
-	golang.org/x/tools v0.1.1 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	go.uber.org/atomic v1.6.0 // indirect
+	go.uber.org/dig v1.12.0 // indirect
+	go.uber.org/multierr v1.5.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
+	golang.org/x/sys v0.0.0-20210903071746-97244b99971b // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
 )
--- main.go
+++ main.go
@@ -1,59 +1,6 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/stretchr/testify/mock"
-
-	// has purpose as of method 1, in mock q&a section, readme
-	//_ "github.com/golang/mock/mockgen/model"
-
-	"github.com/jaytaylor/mockery-example/mocks"
-)
-
 func main() {
-	mockS3 := &mocks.S3API{}
-	//mockS3 := &mocks.MockS3API{}
-
-	mockResultFn := func(input *s3.ListObjectsInput) *s3.ListObjectsOutput {
-		output := &s3.ListObjectsOutput{}
-		output.SetCommonPrefixes([]*s3.CommonPrefix{
-			{
-				Prefix: aws.String("2017-01-01"),
-			},
-		})
-		output.SetContents([]*s3.Object{
-			{
-				Key: aws.String("foo-object"),
-			},
-		})
-		return output
-	}
-
-	mockS3.On("ListObjects",
-		mock.MatchedBy(func(input *s3.ListObjectsInput) bool {
-			return input.Delimiter != nil && *input.Delimiter == "/" && input.Prefix == nil
-		}),
-	).
-		Return(mockResultFn, nil)
-
-	// action
-	listingInput := &s3.ListObjectsInput{
-		Bucket:    aws.String("foo"),
-		Delimiter: aws.String("/"),
-	}
-	listingOutput, err := mockS3.ListObjects(listingInput)
-	if err != nil {
-		panic(err)
-	}
-
-	// print
-	for _, x := range listingOutput.CommonPrefixes {
-		fmt.Printf("common prefix: %+v\n", *x)
-	}
-	for _, x := range listingOutput.Contents {
-		fmt.Printf("content: %+v\n", *x)
-	}
+	//RunS3()
+	RunFx()
 }
